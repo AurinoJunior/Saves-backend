42d066f75c4a46d23445e1920e430337da3071bd
Aurino Geraldo <aurino_aj@hotmail.com>
2019-04-09 14:40:17 -0300

    separando boxes em salas

--- src/server.js
+++ src/server.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose')
 const path = require('path')
 
 const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
 const port = process.env.PORT || 3000
+
+io.on('connection', socket => {
+  socket.on('connectRoom', box => {
+    socket.join(box)
+  })
+})
 
 mongoose.connect(
   'mongodb+srv://dev:dev123@week6-yvzv5.mongodb.net/omnistack?retryWrites=true',
@@ -12,12 +21,18 @@ mongoose.connect(
   }
 )
 
+app.use((req,res, next)=>{
+  req.io = io;
+
+  return next();
+})
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'))
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
   console.log(`O servidor esta rodando na porta ${port}`)
 })
